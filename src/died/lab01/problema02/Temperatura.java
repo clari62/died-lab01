d371d3a4c222baf39cfebf808a202c9f3ca8e3bb
unknown <clariespertino62@gmail.com>
2021-03-24 17:38:34 -0300

    Resolvi el ejercicio 2

---
+++ src/died/lab01/problema02/Temperatura.java
@@ -0,0 +1,83 @@
+package died.lab01.problema02;
+
+public class Temperatura {
+	private double grados;
+	private Escala escala; 
+	
+	public Temperatura () {
+		this.grados = 0;
+		this.escala = Escala.CELCIUS;
+	}
+	public Temperatura(double temperatura, Escala escala) {
+		this.grados=temperatura;
+		this.escala=escala;
+	}
+	public String toString() {
+			String retorno;
+			if(this.escala==Escala.CELCIUS) {
+				retorno=this.grados+"°C";
+			}
+			else retorno=this.grados+"°F";
+			return retorno;
+	}
+	
+	public double asCelcius() {
+		double retorno=0.0;
+		if(this.escala==Escala.CELCIUS) {
+			retorno= this.grados;
+		}
+		else if(this.escala==Escala.FAHRENHEIT) {
+			retorno=(this.grados-32)*5/9;
+		}
+		return retorno;
+	}
+	
+	public double asFahrenheit() {
+		double retorno=0.0;
+	
+		if(this.escala == Escala.FAHRENHEIT) {
+			retorno = this.grados;
+		}
+		else if(this.escala == Escala.CELCIUS) {
+			retorno = this.grados*9/5 + 32;
+		}
+		return retorno;
+	}
+	
+	public void aumentar(Temperatura temperatura) {
+		double nuevaTemp=0.0;
+		if(temperatura.grados > 0.0 ) {
+			if(this.escala==temperatura.escala) {
+				nuevaTemp = this.grados + temperatura.grados;
+			}
+			else if(this.escala==Escala.CELCIUS) {
+				nuevaTemp = this.grados + (temperatura.grados-32)*5/9;
+			}
+			else if(this.escala==Escala.FAHRENHEIT) {
+				nuevaTemp = this.grados + (temperatura.grados*9/5)+32;
+			}
+		}
+		
+		this.grados = nuevaTemp;
+		
+	}
+	public void disminuir(Temperatura temperatura) {
+		double nuevaTemp=0.0;
+		if(temperatura.grados > 0.0 ) {
+			if(this.escala==temperatura.escala) {
+				nuevaTemp = this.grados - temperatura.grados;
+			}
+			else if(this.escala==Escala.CELCIUS) {
+				nuevaTemp = this.grados - (temperatura.grados-32)*5/9;
+			}
+			else if(this.escala==Escala.FAHRENHEIT) {
+				nuevaTemp = this.grados - (temperatura.grados*9/5)+32;
+			}
+		}
+		
+		this.grados = nuevaTemp;
+		
+	}
+
+}
+	
